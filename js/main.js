f29c4d442a838573c838290d31c28fd729160bad
Leo Silverberg <leosilverberg@gmail.com>
2016-03-09 16:05:52 +0000

    Partners and stats

--- js/main.js
+++ js/main.js
@@ -4,31 +4,36 @@ var storedScrollPos;
 var timeout;
 var $rightAnchor;
 
-function setHash(){
+function setHash(event){
   window.location.hash= $rightAnchor;
+
 };
 
 /*going to the project side */
 $('.project-link').bind('click',function(event){
+
       /*stores the scroll position before moving to the page */
-      storedScrollPos = $(window).scrollTop()
+      storedScrollPos = $(window).scrollTop();
 
       /*Fetch the right anchor name */
       $rightAnchor = $(this).attr('href');
 
       /*SCROLLIN' */
+      $('html, body').animate({
+          scrollLeft: $($rightAnchor).offset().left
+      }, 500);
        $('html, body').animate({
-           scrollLeft: $($rightAnchor).offset().left
-       }, 500);
-       $('html, body').animate({
-           scrollTop: $($rightAnchor).offset().top - 50
+           scrollTop: $($rightAnchor).offset().top - 45
        }, 500);
 
+
        /* Stops the anchor from jumping directly to the spot*/
-       event.preventDefault();
+         event.preventDefault();
 
       /*sets the hash of the url to the anchor name after 1 sec, avoiding the jump */
        timeout = window.setTimeout(setHash, 1000);
+       $('html, body').css('overflowY', 'hidden');
+
 
 
  });
@@ -49,4 +54,16 @@ $('.project-link').bind('click',function(event){
          event.preventDefault();
          $rightAnchor = " ";
          timeout = window.setTimeout(setHash, 1000);
+         $('html').css('overflowY', 'auto'); 
   });
+
+
+  /*Color picker for the stats */
+
+var colors = ['120,225,231,', '254,155,143,', '173,180,196,'];
+
+$( ".statnumber" ).each(function() {
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  var random_alpha = (Math.random() * (0.08 - 0.3) + 0.3);
+  $(this).css("background-color", "rgba("+random_color+random_alpha+")");
+});
